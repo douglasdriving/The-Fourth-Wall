4907f72f7650e22482c1f0f074be73c46c231967
douglasdriving <41374220+douglasdriving@users.noreply.github.com>
2024-08-14 13:19:37 +0200

    removed some unused stuff

--- Assets/WordPickup/WordPickup.cs
+++ Assets/WordPickup/WordPickup.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 
 public class WordPickup : MonoBehaviour
@@ -10,21 +6,6 @@ public class WordPickup : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag("Player")) return;
-
-        if (CurrentGameRules.rules.dangerousCharsOn)
-        {
-            string word = wordGO.GetComponentInChildren<TMP_Text>().text;
-            word = word.Trim();
-            word = word.ToUpper();
-            char firstChar = word[0];
-            if (firstChar == CurrentGameRules.rules.dangerousChar)
-            {
-                Debug.Log("you died because you walked over a word with the first letter: " + firstChar + ". the word was: " + word);
-                RespawnSystem.KillPlayerAndReset();
-                return;
-            }
-        }
-
         FindObjectOfType<WordPickupCounter>().AddWord();
         Destroy(wordGO);
     }
