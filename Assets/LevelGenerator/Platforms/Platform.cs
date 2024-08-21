1c287a77042e1cee2bb09b1db131d28922d2c10a
douglasdriving <41374220+douglasdriving@users.noreply.github.com>
2024-08-21 17:51:26 +0200

    more archiving

--- Assets/LevelGenerator/Platforms/Platform.cs
+++ Assets/LevelGenerator/Platforms/Platform.cs
@@ -6,32 +6,4 @@ using UnityEngine;
 public class Platform : MonoBehaviour
 {
   public Transform pieces;
-
-  float percentagePlayerMustLookAwayForActivation = 0.6f;
-  LookAwayChecker playerLookAwayChecker;
-
-  void Awake()
-  {
-    playerLookAwayChecker = GameObject.FindWithTag("Player").GetComponent<LookAwayChecker>();
-  }
-
-  public void HidePlatform()
-  {
-    pieces.gameObject.SetActive(false);
-  }
-
-  void Update()
-  {
-    ActivateIfPlayerIsLookingAway();
-  }
-
-  private void ActivateIfPlayerIsLookingAway()
-  {
-    if (pieces.gameObject.activeInHierarchy) return;
-    bool isPlayerLookingAway = playerLookAwayChecker.IsPlayerLookingAway(transform, percentagePlayerMustLookAwayForActivation);
-    if (isPlayerLookingAway)
-    {
-      pieces.gameObject.SetActive(true);
-    }
-  }
 }
