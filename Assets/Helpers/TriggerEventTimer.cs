24c6466214d7da67df6cc670abb4eda92c2c70c8
douglasdriving <41374220+douglasdriving@users.noreply.github.com>
2024-08-26 12:03:40 +0200

    cleaned up event trigger timer

--- Assets/Helpers/TriggerEventTimer.cs
+++ Assets/Helpers/TriggerEventTimer.cs
@@ -1,47 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-/// <summary>
-/// Triggers an event after a certain amount of time has passed since the trigger event
-/// </summary>
-public class TriggerEventTimer : MonoBehaviour
+namespace Helpers
 {
-
-    [SerializeField] string tagToTriggerFor = "Player";
-    [SerializeField] bool onlyTriggerOnce = true;
-    [SerializeField] float timeBeforeEvent = 30;
-    [SerializeField] UnityEvent eventToToFireAfterTimer;
-    bool hasBeenTriggeredAtLeastOnce = false;
-    float timeSinceTrigger = -1;
-
-    private void OnTriggerEnter(Collider other)
+    /// <summary>
+    /// Triggers a Unity event on a collision with a tagged object after a delay
+    /// </summary>
+    public class TriggerEventTimer : MonoBehaviour
     {
-        if (!other.CompareTag(tagToTriggerFor)) return;
-        if (hasBeenTriggeredAtLeastOnce && onlyTriggerOnce) return;
-        if (hasBeenTriggeredAtLeastOnce && IsTimerRunning()) return;
 
-        hasBeenTriggeredAtLeastOnce = true;
-        timeSinceTrigger = timeBeforeEvent;
-    }
+        [SerializeField] string tagToTriggerFor = "Player";
+        [SerializeField] bool onlyTriggerOnce = true;
+        [SerializeField] float timeBeforeEvent = 30;
+        [SerializeField] UnityEvent eventToToFireAfterTimer;
+        bool hasBeenTriggeredAtLeastOnce = false;
+        float timeSinceTrigger = -1;
 
-    void Update()
-    {
-        if (IsTimerRunning())
+        private void OnTriggerEnter(Collider other)
         {
-            timeSinceTrigger -= Time.deltaTime;
-            bool timerDone = timeSinceTrigger <= 0;
-            if (timerDone)
+            if (!other.CompareTag(tagToTriggerFor)) return;
+            if (hasBeenTriggeredAtLeastOnce && onlyTriggerOnce) return;
+            if (hasBeenTriggeredAtLeastOnce && IsTimerRunning()) return;
+
+            hasBeenTriggeredAtLeastOnce = true;
+            timeSinceTrigger = timeBeforeEvent;
+        }
+
+        void Update()
+        {
+            if (IsTimerRunning())
             {
-                timeSinceTrigger = -1;
-                eventToToFireAfterTimer.Invoke();
+                timeSinceTrigger -= Time.deltaTime;
+                bool timerDone = timeSinceTrigger <= 0;
+                if (timerDone)
+                {
+                    timeSinceTrigger = -1;
+                    eventToToFireAfterTimer.Invoke();
+                }
             }
         }
-    }
 
-    bool IsTimerRunning()
-    {
-        return hasBeenTriggeredAtLeastOnce && timeSinceTrigger > 0;
+        bool IsTimerRunning()
+        {
+            return hasBeenTriggeredAtLeastOnce && timeSinceTrigger > 0;
+        }
     }
 }
