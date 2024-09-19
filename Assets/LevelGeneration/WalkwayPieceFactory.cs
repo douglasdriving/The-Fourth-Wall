447dce1e87a6a9ff8640a81757da2cee63db1891
douglasdriving <41374220+douglasdriving@users.noreply.github.com>
2024-09-19 15:07:56 +0900

    Update WalkwayPieceFactory.cs

--- Assets/LevelGeneration/WalkwayPieceFactory.cs
+++ Assets/LevelGeneration/WalkwayPieceFactory.cs
@@ -15,12 +15,32 @@ namespace LevelGeneration
     [SerializeField] GameObject walkwayPiecePrefab;
     [SerializeField] LevelPieceMolds levelPieceMolds;
 
+    /// okay so this was a little bit wierd, it just keeps going up and up <summary>
+    /// instead we could create an "ideal height" for the piece, and then pick the point that is as close to that as possible but still not blocked by the other piece
     public GameObject InstantiatePieceAbovePos(Vector3 finalPos, Quaternion finalRot, Transform lastPiece, string pieceWord)
     {
-      float heightAbovePrevious = 0.3f; //magic?
-      Vector3 spawnPos = finalPos;
-      spawnPos.y = lastPiece.position.y + heightAbovePrevious;
-      GameObject piece = GameObject.Instantiate(walkwayPiecePrefab, spawnPos, Quaternion.identity);
+
+      float idealHeightAboveFinalPos = 1f;
+      float idealHeight = finalPos.y + idealHeightAboveFinalPos;
+      Vector2 ideal2DPos = new Vector2(finalPos.x, idealHeight);
+      float minDiffFromPreviousPos = 1f;
+      Vector2 previousPiece2DPos = new Vector2(lastPiece.position.x, lastPiece.position.y);
+      float distanceFromPrevious = Vector2.Distance(ideal2DPos, previousPiece2DPos);
+      bool isBlockedByPrevious = distanceFromPrevious < minDiffFromPreviousPos;
+      Vector2 spawnPos2D = ideal2DPos;
+      if (isBlockedByPrevious)
+      {
+        Vector2 directionFromPreviousPosToIdealPos2D = (ideal2DPos - previousPiece2DPos).normalized;
+        spawnPos2D = previousPiece2DPos + directionFromPreviousPosToIdealPos2D * minDiffFromPreviousPos;
+        Debug.Log("blocked by previous, moving by vector from last 2d pos " + directionFromPreviousPosToIdealPos2D * minDiffFromPreviousPos);
+      }
+      Vector3 spawnPos = new Vector3(spawnPos2D.x, spawnPos2D.y, finalPos.z);
+
+      // float heightAbovePrevious = 0.3f; //magic?
+      // Vector3 spawnPos = finalPos;
+      // spawnPos.y = lastPiece.position.y + heightAbovePrevious;
+
+      GameObject piece = Instantiate(walkwayPiecePrefab, spawnPos, Quaternion.identity);
       piece.transform.up = Vector3.back;
       piece.GetComponentInChildren<TMP_Text>().text = pieceWord;
       piece.GetComponent<LevelPiecePositioner>().MoveWithSimpleAnimation(finalPos, finalRot);
