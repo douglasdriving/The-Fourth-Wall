76cc6d830e1e4b61415e2fe51a4fcd733b40a032
douglasdriving <41374220+douglasdriving@users.noreply.github.com>
2024-10-12 21:25:51 +0900

    and rail done

--- Assets/LevelGeneration/AndRail/AndRailRider.cs
+++ Assets/LevelGeneration/AndRail/AndRailRider.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 /// <summary>
@@ -9,8 +10,8 @@ using UnityEngine;
 public class AndRailRider : MonoBehaviour
 {
     //balance variables
-    [SerializeField] float totalRideTime = 0.5f;
     [SerializeField] float distanceFromEndPlayerWillBeDroppedAt = 0.5f;
+    [SerializeField] float rideSpeed = 10;
 
     //player
     Transform player;
@@ -21,11 +22,17 @@ public class AndRailRider : MonoBehaviour
     Vector3 playerStartPos;
     Vector3 playerEndPos;
 
+    //can start ride
+    [SerializeField] GameObject ridePromptCanvas;
+    bool playerIsInRideStartArea = false;
+    bool isRiding = false;
+
     void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         playerController = player.GetComponent<FirstPersonController>();
         playerRb = player.GetComponent<Rigidbody>();
+        ridePromptCanvas.SetActive(false);
     }
 
     public void UpdatePlayerStartAndEnd(Vector3 _railStart, Vector3 _railEnd)
@@ -39,33 +46,80 @@ public class AndRailRider : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            StartCoroutine(Ride());
+            SetRideStartEnabled(true);
         }
     }
 
-    IEnumerator Ride()
+    void OnTriggerExit(Collider other)
     {
-        SetPlayerMovementAndGravityEnabled(false);
-        yield return MoveAlongRail();
+        if (other.CompareTag("Player"))
+        {
+            SetRideStartEnabled(false);
+        }
+    }
+
+    void SetRideStartEnabled(bool shouldBeEnabled)
+    {
+        playerIsInRideStartArea = shouldBeEnabled;
+        ridePromptCanvas.SetActive(shouldBeEnabled);
+    }
+
+    void Update()
+    {
+        if (playerIsInRideStartArea && Input.GetKeyDown(KeyCode.E))
+        {
+            StartRide();
+        }
+        else if (isRiding && Input.GetKeyUp(KeyCode.E))
+        {
+            StopRiding();
+        }
+        else if (isRiding)
+        {
+            UpdateRide();
+        }
+    }
+
+    private void StopRiding()
+    {
+        isRiding = false;
         SetPlayerMovementAndGravityEnabled(true);
     }
 
-    IEnumerator MoveAlongRail()
+    private void UpdateRide()
     {
-        player.position = playerStartPos;
-        float timeOnRail = 0;
-        while (timeOnRail < totalRideTime)
+        MovePlayerForwardAlongRail();
+        bool hasReachedEnd = Vector3.Distance(player.position, playerEndPos) < 0.4f;
+        if (hasReachedEnd)
         {
-            float progress = timeOnRail / totalRideTime;
-            player.position = Vector3.Lerp(playerStartPos, playerEndPos, progress);
-            timeOnRail += Time.deltaTime;
-            yield return null;
+            EndRide();
         }
+    }
+
+    private void EndRide()
+    {
         player.position = playerEndPos + Vector3.forward * distanceFromEndPlayerWillBeDroppedAt;
+        SetPlayerMovementAndGravityEnabled(true);
+        isRiding = false;
+    }
+
+    private void MovePlayerForwardAlongRail()
+    {
+        Vector3 rideDir = (playerEndPos - playerStartPos).normalized;
+        player.position += rideDir * rideSpeed * Time.deltaTime;
+    }
+
+    private void StartRide()
+    {
+        ridePromptCanvas.SetActive(false);
+        SetPlayerMovementAndGravityEnabled(false);
+        isRiding = true;
+        player.position = playerStartPos;
     }
 
     void SetPlayerMovementAndGravityEnabled(bool enabled)
     {
+        if (playerController.canMove == enabled && playerRb.useGravity == enabled) return;
         playerController.canMove = enabled;
         playerRb.useGravity = enabled;
     }
