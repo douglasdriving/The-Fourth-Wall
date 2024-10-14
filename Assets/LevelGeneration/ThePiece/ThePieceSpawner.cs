d45f4b52e17ca779c81517df0d30cd5cf14fba6e
douglasdriving <41374220+douglasdriving@users.noreply.github.com>
2024-10-14 19:15:21 +0900

    clean

--- Assets/LevelGeneration/ThePiece/ThePieceSpawner.cs
+++ Assets/LevelGeneration/ThePiece/ThePieceSpawner.cs
@@ -20,14 +20,14 @@ namespace LevelGeneration.ThePiece
 
         private static void StartAnimation(Vector3 entryPoint, GameObject piece)
         {
-            LevelPiecePositioner positioner = piece.GetComponent<LevelPiecePositioner>(); //need THE piece positioner??
+            LevelPiecePositioner positioner = piece.GetComponent<LevelPiecePositioner>();
             Vector3 finalPos = entryPoint + Vector3.down * 0.5f;
             positioner.MoveWithSimpleAnimation(finalPos, Quaternion.identity);
         }
 
         private GameObject SpawnAboveFinalPos(Vector3 entryPoint)
         {
-            Vector3 spawnPosition = entryPoint + Vector3.up * 3.5f + Vector3.left * 2.7f + Vector3.forward * 2; //magic
+            Vector3 spawnPosition = entryPoint + Vector3.up * 3.5f + Vector3.left * 2.7f + Vector3.forward * 2;
             Quaternion spawnRotation = Quaternion.Euler(0, 90, 270);
             GameObject piece = Instantiate(thePiecePrefab, spawnPosition, spawnRotation);
             return piece;
