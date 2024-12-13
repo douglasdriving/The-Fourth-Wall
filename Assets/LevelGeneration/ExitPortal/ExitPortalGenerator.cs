6ff5d5bb4d81c0980ddbdabfbfae62b897f6ab60
douglasdriving <41374220+douglasdriving@users.noreply.github.com>
2024-12-13 15:22:10 +0900

    better portal spawn timing

--- Assets/LevelGeneration/ExitPortal/ExitPortalGenerator.cs
+++ Assets/LevelGeneration/ExitPortal/ExitPortalGenerator.cs
@@ -10,18 +10,15 @@ namespace LevelGeneration
     {
         [SerializeField] GameObject exitPortalPrefab;
         [SerializeField] float portalHeightAbovePlatform = 1.5f;
+        [SerializeField] float timeBetweenPieceAndPortalSpawn = 0.8f;
 
         public IEnumerator GenerateExitPortalAfterDelay(float delay)
         {
             yield return new WaitForSeconds(delay);
-            GeneratePieceWithPortal();
-        }
-
-        private void GeneratePieceWithPortal()
-        {
             GameObject portalLevelPiece = FindObjectOfType<LevelGenerator>().SpawnNextPiece("");
             LevelPiecePositioner piecePositioner = portalLevelPiece.GetComponent<LevelPiecePositioner>();
             Vector3 portalPos = piecePositioner.targetPos + Vector3.up * portalHeightAbovePlatform;
+            yield return new WaitForSeconds(timeBetweenPieceAndPortalSpawn);
             Instantiate(exitPortalPrefab, portalPos, piecePositioner.targetRot);
         }
     }
